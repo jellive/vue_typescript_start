c8a30a2d09de72ffcd3c0400b2561111125d25cc
Jell PD <jellive7@gmail.com>
2019-07-18 22:42:25 +0900

    make vue file with class style.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <message/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import message from '@/components/message.vue';
 
-@Component({
+@Component({ // decorator. just function.
   components: {
-    HelloWorld,
+    // HelloWorld,
+    message,
   },
 })
 export default class Home extends Vue {}
