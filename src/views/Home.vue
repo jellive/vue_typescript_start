74365bc203d53a1e43aa4e6d1ebb9330e2ca8ad1
Jell PD <jellive7@gmail.com>
2019-07-18 23:45:23 +0900

    add provice/inject comment.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,7 +16,7 @@ import children from '@/components/children.vue'
   },
 })
 export default class Home extends Vue {
-  @Provide('message') msg: string = 'provide/inject example'
+  @Provide('message') msg: string = 'provide/inject example' // provide는 부모를 여러번 거쳐도 받을 수 있다. 또한 자식 컴포넌트에게 template에서 명시하지 않아도 된다.
 
 }
 </script>
