591190460bd420ced3364d31467e76bac79a4ed8
Jell PD <jellive7@gmail.com>
2019-07-18 23:38:19 +0900

    add emit.

--- src/components/children.vue
+++ src/components/children.vue
@@ -1,25 +1,18 @@
 <template>
 <div>
-    <p>
-        {{alertMessage}}
-    </p>
-    {{parentMessage}}
+    <button @click="counter">자식에서 숫자를 증가시킵니다.</button>
 </div>
 </template>
 
 <script lang="ts">
-import {Component, Prop, Vue, Watch} from 'vue-property-decorator'
+import {Component, Prop, Vue, Watch, Emit} from 'vue-property-decorator'
 
 @Component
 export default class Children extends Vue {
-    @Prop() public parentMessage?: string
-    alertMessage: string = ''
-
-    @Watch('parentMessage', {immediate: true, deep: true}) // immediate는 변경 즉시 호출 여부, deep은 객체 내부의 일부 값만 바뀌어도 호출할 지 여부. watch확인하려면 option은 지워야 함.
-    update(value: string, oldValue: string) {
-        this.alertMessage = '메세지를 업데이트했습니다.'
+    @Emit() // 이름이 같으면 인자값을 안넣어도 됨.
+    counter() {
+        console.log('count')
     }
-
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,29 +3,28 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
     <!-- <message/> -->
-    <children :parentMessage="message"/>
-    <button @click="changeMessage">메세지를 바꿔줄께요.</button>
+    <!-- <children :parentMessage="message"/> -->
+    <children @counter="counter"/>
+      <p>
+        부모에서 숫자를 보여줍니다. : {{count}}
+      </p>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-// import message from '@/components/message.vue';
 import children from '@/components/children.vue'
 
 @Component({ // decorator. just function.
   components: {
-    // HelloWorld,
-    // message,
     children,
   },
 })
 export default class Home extends Vue {
-  public message: string = 'hello world'
+  count: number = 0
 
-  changeMessage() {
-    this.message = 'change'
+  counter() {
+    this.count++
   }
 }
 </script>
