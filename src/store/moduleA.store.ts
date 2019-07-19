d02f766ee330f40653dfe496759399f4abefe152
PD Jell <jellive7@gmail.com>
2019-07-19 13:46:04 +0900

    vuex-module-decorator (store in class style).

--- src/store/moduleA.store.ts
+++ src/store/moduleA.store.ts
@@ -1,28 +1,52 @@
-import { Module } from "vuex";
-import { RootState } from './store'
+// Class Style (with vuex-module-decorators)
+import {Module, VuexModule, Mutation, Action} from 'vuex-module-decorators'
 
-interface moduleA {
-    data: string
+@Module({namespaced: true, name: 'moduleA'})
+export default class ModuleA extends VuexModule {
+  data: string = 'moduleA'
+
+  @Mutation
+  setData (data: string) {
+    this.data = data
+  }
+
+  @Action
+  editData (data: string) {
+    this.context.commit('setData', data)
+  }
+
+  get moduleAData() {
+    return this.data
+  }
 }
 
-const module: Module<moduleA, RootState> = {
-    namespaced: true,
-    state: {
-        data: 'moduleA'
-    },
-    mutations: {
-      setData(state, data: string) { // state의 자료형은 이미 정의되어 있어서 안 넣어도 됨.
-        state.data = data
-      }
-    },
-    actions: {
-      setRootData({commit}, data: string) {
-        commit('setData', data)
-      }
-    },
-    getters: {
-      data: (state) => state.data
-    }
-}
+// Object Style (basic)
 
-export default module
+// import { Module } from "vuex";
+// import { RootState } from './store'
+
+// interface moduleA {
+//     data: string
+// }
+
+// const module: Module<moduleA, RootState> = {
+//     namespaced: true,
+//     state: {
+//         data: 'moduleA'
+//     },
+//     mutations: {
+//       setData(state, data: string) { // state의 자료형은 이미 정의되어 있어서 안 넣어도 됨.
+//         state.data = data
+//       }
+//     },
+//     actions: {
+//       setRootData({commit}, data: string) {
+//         commit('setData', data)
+//       }
+//     },
+//     getters: {
+//       data: (state) => state.data
+//     }
+// }
+
+// export default module
